f2c35777b1be9e836cd9ca696ab62b8cd718c622
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-08 18:03:50 +0330

    Finished Navbar, hero and highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,44 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { watchImg, rightImg } from "../utils";
+import VideoCard from "./VideoCard";
 
 const Highlights = () => {
+	useGSAP(() => {
+		gsap.to("#title", {
+			opacity: 1,
+			y: 0,
+			duration: 1,
+		});
+
+		gsap.to(".link", {
+			opacity: 1,
+			y: 0,
+			duration: 1,
+			stagger: 0.2,
+		})
+	}, [])
+
 	return (
-		<div>Highlights</div>
+		<section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+			<div className="screen-max-width">
+				<div className="mb-12 w-full md:flex items-end justify-between">
+					<h1 id="title" className="section-heading">Get the highlights</h1>
+
+					<div className="flex flex-wrap items-end gap-5">
+						<p className="link">
+							Watch the film
+							<img src={watchImg} alt="watch" className="ml-2"/>
+						</p>
+						<p className="link">
+							Watch the event
+							<img src={rightImg} alt="right" className="ml-2"/>
+						</p>
+					</div>
+				</div>
+				<VideoCard />
+			</div>
+		</section>
 	)
 }
 

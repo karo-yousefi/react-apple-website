0422822f0ad8feaf942331c9f31b7421a5314190
Karo Yousefi <karoyousefi.80@gmail.com>
2024-10-09 20:18:36 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'react-apple-website',
+  base: '/react-apple-website',
   plugins: [react()],
 })
